d861b25d670bb89bb0d107f3cbeb0872bce0a0e2
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2022-05-05 21:35:59 -0300

    projeto música itunes

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const button = document.getElementById("add")
+const searchInput = document.getElementById('search');
+const searchButton = document.getElementById('search-button');
+const searchResults = document.getElementById('search-results');
+const url = 'https://itunes.apple.com/search?';
 
-let num = 0;
 
-const add = () => {
-  num++;
-  const number = document.getElementById("number")
-  console.log(number)
-  number.innerHTML = num;
+function createAudioElement(url) {
+  const audio = document.createElement('audio');
+  audio.src = url;
+  audio.controls = true;
+  searchResults.appendChild(audio);
 }
 
-button.addEventListener("click", add);
+searchButton.addEventListener('click', () => {
+  console.log(url + `term=${searchInput.value}`);
+
+  // createAudioElement();
+});
